f995d00543fd86e08f19332764328cf6e26db1f4
AmalFaisalkhann <amalf.khan2@gmail.com>
2025-07-02 10:08:54 +0500

    Add SuperUser screen with user management functionality

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Signup from './Auth/Signup';
 import ForgotPassword from './Auth/ForgotPassword';
 import Userroles from './Userroles/Userroles';
 import Productowner from './Dashboard/Productowner';
+import SuperUser from './Dashboard/SuperUser';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Userroles />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Signup" element={<Signup />} />
-      <Route path="/ForgotPassward" element={<ForgotPassword />} />
-      <Route path="*" element={<Navigate to="/" />} />
-      <Route path="/Productowner" element={<Productowner />} />
+     <Route path="/" element={<Userroles />} />
+<Route path="/Login" element={<Login />} />
+<Route path="/Signup" element={<Signup />} />
+<Route path="/ForgotPassward" element={<ForgotPassword />} />
+<Route path="/Productowner" element={<Productowner />} />
+<Route path="/SuperUser" element={<SuperUser />} />
+
+<Route path="*" element={<Navigate to="/" />} />  // Put this last
 
     </Routes>
   );
