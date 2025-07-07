83f35e01a9e83393f4b07620e4a059c759e1822a
aliraza727272 <u2022664@giki.edu.pk>
2025-07-07 16:24:18 +0500

    done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// src/App.js
+
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Auth/Login';
+import Signup from './Auth/Signup';
+import ForgotPassword from './Auth/ForgotPassword';
+import Userroles from './Userroles/Userroles';
+import Productowner from './Dashboard/Productowner';
+import Landingpage from './landingpage';
+import EnterWorkspace from './Dashboard/EnterWorkspace';
+import Workspace from './Dashboard/Workspace';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landingpage />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Signup" element={<Signup />} />
+      <Route path="/ForgotPassward" element={<ForgotPassword />} />
+      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/Productowner" element={<Productowner />} />
+      <Route path="/Workspace" element={<Workspace />} />
+      <Route path="/EnterWorkspace" element={<EnterWorkspace />} />
+
+
+    </Routes>
+  );
+}
+
+export default App;
